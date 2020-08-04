75be4990b4a8e21746075bab73a31bf446035ae6
alienwareone <beni@mailboxes.ch>
2020-08-04 23:40:11 +0200

    Init

---
+++ EfCoreOrderByIndexOfBug/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EfCoreOrderByIndexOfBug
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(LoggerFactory.Create(x => x
+                    .AddConsole()
+                    .AddFilter(y => y >= LogLevel.Debug)))
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ EfCoreOrderByIndexOfBug/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace EfCoreOrderByIndexOfBug
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var dbContextOptionsBuilder = new DbContextOptionsBuilder();
+            dbContextOptionsBuilder.UseSqlServer("Data Source = .\\SQLEXPRESS; Database = EfCoreOrderByIndexOfBug; Integrated Security = True;");
+
+            var applicationDbContext = new ApplicationDbContext(dbContextOptionsBuilder.Options);
+            applicationDbContext.Database.EnsureCreated();
+
+            if (!applicationDbContext.Categories.Any())
+            {
+                for (var i = 0; i < 20; i++)
+                {
+                    applicationDbContext.Categories.Add(new Category { Name = $"Name {i}" });
+                }
+                applicationDbContext.SaveChanges();
+            }
+
+            var searchTerm = "Na";
+
+            var names = applicationDbContext.Categories
+                .Select(x => x.Name)
+                .Distinct()
+                .OrderBy(x => x.IndexOf(searchTerm))
+                .ThenBy(x => x)
+                .Take(10)
+                .ToList();
+        }
+    }
+}
